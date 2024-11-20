cc7ff9a417f37245bbc81c3ea71aa88146df9811
MYMEILE <529266369@qq.com>
2024-11-20 17:06:20 +0800

    add

--- src/bot/utils/aptos_price.ts
+++ src/bot/utils/aptos_price.ts
@@ -2,11 +2,11 @@ import axios from 'axios'
 
 export async function getAptPrice(): Promise<number | undefined> {
   // const url = 'https://api.binance.com/api/v3/ticker/price?symbol=APTUSDT'
-  const url = 'https://demo.deagfent.ai/api/v1/plug/apt_gas_price'
+  const url = 'https://demo.deagent.ai/api/v1/plug/apt_gas_price'
 
   try {
     const response = await axios.get(url)
-    return Number.parseFloat(response.data.price)
+    return Number.parseFloat(response.data)
   }
   catch (error) {
     console.error('Error fetching APT price:', error)
