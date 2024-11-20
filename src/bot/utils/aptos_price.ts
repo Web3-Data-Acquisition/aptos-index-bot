b7aa5205b33ce072c2f17d4d6b262e87a93d9daf
MYMEILE <529266369@qq.com>
2024-11-20 16:19:59 +0800

    add

--- src/bot/utils/aptos_price.ts
+++ src/bot/utils/aptos_price.ts
@@ -2,8 +2,14 @@ import axios from 'axios'
 
 export async function getAptPrice(): Promise<number | undefined> {
   const url = 'https://api.binance.com/api/v3/ticker/price?symbol=APTUSDT'
+
+  const proxyConfig = {
+    host: '205.198.65.182',
+    port: 38080,
+    protocol: 'http', // 如果你的代理支持 https，这里也可以是 'https'
+  }
   try {
-    const response = await axios.get(url)
+    const response = await axios.get(url, { proxy: proxyConfig })
     return Number.parseFloat(response.data.price)
   }
   catch (error) {
